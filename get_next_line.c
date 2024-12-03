c1b02fa1d7fe24cec4a0ef6ed377e4e09dde3b5a
mikkosuokas <mikesuokas@gmail.com>
2024-12-03 15:14:44 +0200

    trying different approach

--- get_next_line.c
+++ get_next_line.c
@@ -1,52 +1,47 @@
 #include "get_next_line.h"
 
-char	*make_line(char *line, char *buffer)
+char	*join_to_buf(char *result, char *buffer)
 {
-	char	*new_line;
+	char	*new;
 
-	new_line = ft_strjoin(buffer, line);
-	free (buffer);
-	return (new_line);
+	new = ft_strjoin(buffer, result);
+	free(buffer);
+	return (new);
 }
 
-char	*get_line(int fd, char *buffer)
+char	*get_line(int fd, char *result)
 {
-	char	*line;
-	int		byte_check;
+	char	*buffer;
+	int		byte_count;
 
-	byte_check = 1;
-	line = malloc((BUFFER_SIZE + 1) * sizeof(char));
-	if (line == NULL)
+	byte_count = 1;
+	buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (buffer == NULL)
 		return (NULL);
-	while (byte_check > 0)
+	while (byte_count > 0)
 	{
-		byte_check = read(fd, buffer, BUFFER_SIZE);
-		if (byte_check == -1)
+		byte_count = read(fd, buffer, BUFFER_SIZE);
+		if (byte_count == -1)
 		{
-			free(line);
+			free(buffer);
 			return (NULL);
 		}
-		buffer = make_line(buffer, line);
-		if (ft_strchr(buffer, "\n"))
+		buffer[byte_count] = 0;
+		result = join_to_buf(result, buffer);
+		if (ft_strchr(buffer, '\n'))
 			break ;
 	}
-	free(line);
-	return (buffer);
+	free(buffer);
+	return (result);
 }
 
-char	*clean_up(char *line)
+char	*get_next_line(int fd)
 {
-	
-}
-
-char *get_next_line(int fd)
-{
-	static char	*buffer; //empty buffer at the beginning
-	char		*clean_line;
+	char	*buffer;
+	char	*line;
 
 	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, buffer, BUFFER_SIZE) < 0)
 		return (NULL);
 	buffer = get_line(fd, buffer);
-	clean_line = clean_up(buffer);
-
+	
 }
